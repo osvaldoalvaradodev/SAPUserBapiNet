a05f9dd8799649e3aa42d593f1c8aa23eb36c027
Nicolás Prieto <nicolas@prieto.in>
2020-08-28 14:00:53 -0400

    Agregar archivos de proyecto.

---
+++ SAPUserBapiNet/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAPUserBapiNet
+{
+    public class Class1
+    {
+    }
+}
